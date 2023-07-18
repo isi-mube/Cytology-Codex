f90df1b463dfb4d67b5695bf83edc5be9bbe7a75
isi-mube <isi.mube@protonmail.com>
2023-07-18 19:08:32 +0200

    up2date

---
+++ 03_app/app_without_API.py
@@ -0,0 +1,303 @@
+import os
+import streamlit as st
+import tensorflow as tf
+import openai
+from PIL import Image
+from tensorflow.keras.preprocessing import image
+import numpy as np
+
+# API key for GPT-3
+openai.api_key = 'YOUR-API'
+
+# data path
+data_path = os.path.join('C:\\Users\\apisi\\01. IronData\\01. GitHub\\03. Projects\\08_cells_at_work', '01_data')
+
+# salivary gland model
+salivary_gland = os.path.join(data_path, '01_salivary_gland')
+checkpoint_path = os.path.join(salivary_gland, "00_epochs", "save_at_23.keras")
+salivary_model = tf.keras.models.load_model(checkpoint_path)
+
+# page configuration
+st.set_page_config(page_title='Cytology Codex', layout='wide')
+top_sidebar_placeholder = st.sidebar.empty()
+
+# image for Home page
+top_sidebar_placeholder.markdown('''
+<p align="center">
+  <img src="https://imgtr.ee/images/2023/07/14/b1deb27bdd471bb2b8fc5374d9d9cca5.png" width="100%" alt="Cytology Image">
+  <br>
+</p>
+''', unsafe_allow_html=True)
+
+# sidebar
+st.sidebar.title('Navigation')
+page = st.sidebar.radio('Select a Page', ['Home', 'Diagnosis', 'Chapter I: Cytology', 'Chapter II: Data', 'Chapter III: Neural Networks'])
+
+with st.spinner('Loading page...'):
+    if page == 'Home':
+        st.title('Cytology Codex')
+        st.header('The Digital Tome of Cytology I: Deep Learning & Neural Networks Transmutation')
+        st.markdown('''
+        <p align="center">
+          <img src="https://cdn.discordapp.com/attachments/1129416761922568212/1130371846781534289/isi.mube_a_closed_spellbook_tome_that_speaks_about_Python_Neura_fcd7bb99-c40d-429c-a8f3-9f2bbdad3d7e.png" width="36%" alt="Cytology Image">
+          <br>
+          <small><em>Behold, the Digital Tome of Cytology, revealing microscopic marvels and cellular untold tales.</em></small>
+        </p>
+        ''', unsafe_allow_html=True)
+        
+    if page == 'Diagnosis':
+        # title
+        st.title("Diagnosis and Report")
+
+        # classes dictionary
+        class_salivary = {i: name.replace('_', ' ').title() for i, name in enumerate(['acinar_carcinoma', 'adenoid_cystic_carcinoma', 'benign', 'pleomorphic_adenoma', 'warthin_tumor'])}
+
+        # organ sample and upload image
+        organ_sample = st.selectbox('Select the organ sample:', ['Salivary Gland', 'Sample 2', 'Sample 3'])
+        uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png"])
+
+        if uploaded_file is not None:
+            img = Image.open(uploaded_file).convert("RGB")
+            img_resized = img.resize((256, 256))
+            img_array = np.expand_dims(image.img_to_array(img_resized), axis=0)
+
+            # Progress bar
+            progress_bar = st.progress(0)
+            for i in range(100):
+                progress_bar.progress(i + 1)
+            st.text("Complete!")
+
+            with st.spinner('Predicting...'):
+                predictions = salivary_model.predict(img_array)
+                class_idx = np.argmax(predictions[0])
+                class_prob = predictions[0][class_idx]
+                class_sg = class_salivary[class_idx] 
+
+            # result presentation
+            col1, col2 = st.columns(2)
+            with col1:
+                col1.image(img, caption='Uploaded Image.', use_column_width=True)
+            with col2:
+                col2.markdown(f"<h2 style='text-align: center; color: green; display: inline-block;'>Diagnosis: {class_sg}</h2>", unsafe_allow_html=True)
+                col2.markdown(f"#### Confidence: **{class_prob*100:.2f}%**")
+                col2.markdown("### Cytology Consultant", unsafe_allow_html=True)
+
+                # AI response for cytological features and malignancy risk
+                with col2.container():
+                    with st.spinner('Consulting with AI...'):
+                        response = openai.ChatCompletion.create(
+                            model="gpt-3.5-turbo",
+                            messages=[
+                                {"role": "system", "content": "You are a helpful assistant."},
+                                {"role": "user", "content": f"Which are the key cytological features and malignancy risk for {class_sg} in the {organ_sample}?"}
+                            ]
+                        )
+                    col2.markdown(f"Cytology Consultant: {response['choices'][0]['message']['content']}")
+                    col2.markdown("###### AI powered by Chat GPT-3", unsafe_allow_html=True)
+
+            with col1:
+                # button queries
+                button_queries = {
+                    "Useful IHQ": f"What are the most frequently used immunohistochemical markers for {class_sg}?",
+                    "Differential Diagnosis": f"What are the differential diagnoses for {class_sg}?"
+                }
+                # button text
+                for button_text, query in button_queries.items():
+                    if st.button(button_text):
+                        with st.spinner('Consulting with AI...'):
+                            response = openai.ChatCompletion.create(
+                                model="gpt-3.5-turbo",
+                                messages=[
+                                    {"role": "system", "content": "You are a helpful assistant."},
+                                    {"role": "user", "content": query}
+                                ]
+                            )
+                        col1.markdown(f"Cytology Consultant: {response['choices'][0]['message']['content']}")
+
+                # empty box for the user
+                user_input = st.text_input("How can I help you?", key="user_input")
+                if user_input:
+                    with st.spinner('Consulting with AI...'):
+                        response = openai.ChatCompletion.create(
+                            model="gpt-3.5-turbo",
+                            messages=[
+                                {"role": "system", "content": "You are a helpful assistant."},
+                                {"role": "user", "content": user_input}
+                            ]
+                        )
+                    col1.markdown(f"Cytology Consultant: {response['choices'][0]['message']['content']}")
+                
+    elif page == 'Chapter I: Cytology':
+        st.markdown('''
+        <h1 id="about-cytology">About Cytology</h1>
+        <p align="center">
+          <img src="https://cdn.discordapp.com/attachments/1129416761922568212/1130370346319609926/isi.mube_an_open_spellbook_featuring_in_their_pages_cytology_im_e604e964-a907-453f-b855-4d34c82382be.png" width="20%">
+        </p>
+        <h2>Glossary</h2>
+        <p align="justify">Let´s define first a few key terms:</p>
+        <ul>
+            <li><strong>Cytology:</strong> This is the study of individual cells to detect abnormalities, including cancer. It's a type of sample method that provides a less invasive alternative to biopsies, enabling early diagnosis and treatment initiation, and improving health outcomes.</li>
+            <li><strong>Cytopathology:</strong> A specialized field, nested in pathology, that looks at diseases on the cellular level. Professionals of cytopathology include Cytotechnologists & Cytopathologists, focusing on screening, interpretation, and diagnosis of diverse cell samples.</li>
+            <li><strong>Digital Pathology:</strong> This involves digitizing pathology slides, allowing the use of image-based information for diagnosis, research, and teaching. Digital Pathology includes not only the digitalization of histology and cytology slides but also the automatization, technology, and tools of all preanalytical, analytical and post-analytical processes in a pathology department</li>
+        </ul>
+
+        <h2>Challenges in Digital Cytology</h2>
+        <p align="justify">In digital cytology, we face a unique challenge. Unlike in histology, where cells maintain their flat structure (like a single layer of bricks on a wall), cytology samples can be more like a pile of bricks dumped out of a bucket. These cells in suspension no longer hold their original formation, making diagnosis more complex and time-consuming because it requires mastery of pattern recognition. Furthermore, due to these additional dimensions, digitizing these cell images requires even more storage space.</p>
+        <br>
+        <p align="center">
+        <img src="https://imgtr.ee/images/2023/07/15/ecaf6fb5b117693590759e95118a8150.jpeg" width="26%">
+        <br>
+        <small><em>Thyroid, papillar carcinoma. Same tumor, different methods and different features. On the left, histology (1-dimensional thin layer), and on the right, cytology (three-dimensional in suspension cells).</em></small>
+        </p>
+
+        <h2>Personal Journey and Perspectives on Cytology</h2>
+        <p align="justify">My past 5 years of work have been all around Cytology; it involved screening and diagnosis of numerous cytology specimens, quality control, and engaging in both teaching and research, including Digital Pathology publications.</p>
+
+        <p align="justify">One significant barrier to the digitalization of Cytological samples is the final size. As previously explained, the cells in Cytology are not flat, unlike in Histology, but three-dimensional. This complexity typically necessitates a Z-stack scanning of the slides to capture all focal points, resulting in large digital files.</p>
+
+        <p align="justify">Despite this challenge, I firmly believe that Machine Learning and Deep Learning models can be implemented in Cytology images, bypassing the need for complete scan, hence one of the most challenging aspects of the digitalization process.</p>
+        ''', unsafe_allow_html=True)
+                    
+    elif page == 'Chapter II: Data':
+        st.markdown('''
+        <div>
+        <h1 id="about-the-data">About the Data</h1>
+        <p align="center">
+          <img src="https://cdn.discordapp.com/attachments/1129416761922568212/1130370155399094293/isi.mube_an_open_spellbook_featuring_in_their_pages_data_charts_d4f6523e-ff38-4e94-a11c-cd151d7087e2.png" width="20%">
+        </p>
+        <p style="text-align:justify">Gathering and extracting sufficient cytology image data for the project was a challenging step due to the lack of publicly available images, slides, and online microscopes.</p>
+
+        <h2 id="first-idea">First Idea</h2>
+
+        <p style="text-align:justify">The original idea was to use my own data, given my direct access to it as a professional in the field. However, the process of manually selecting and capturing the images using my phone camera and microscope proved to be time-consuming and would have necessitated extensive preprocessing. This was not the best approach for an ambitious three-week project.</p>
+
+        <div style="display: flex; justify-content: center;">
+        <div style="width: 20%; padding: 1%;">
+        <p align="center">
+            <img src="https://imgtr.ee/images/2023/07/15/bb05317d3b2f007d0496feed3f64e146.jpeg" style="width: 100%;">
+        </p>
+        </div>
+        <div style="width: 20%; padding: 1%;">
+        <p align="center">
+            <img src="https://imgtr.ee/images/2023/07/15/27944d522ee57766cab7f761b43445ec.jpeg" style="width: 100%;">
+        </p>
+        </div>
+        </div>
+
+        <h2 id="second-idea">Second Idea</h2>
+
+        <p style="text-align:justify">Another option I considered was using images from book atlases and other resources I had accumulated over my years of practice. While these images provided a potential solution, their limited variety and quantity posed challenges for training a robust model.</p>
+
+        <p align="center">
+        <img src="https://imgtr.ee/images/2023/07/15/c135b4d7a3bb85f798846883f3729175.jpeg" width="26%">
+        </p>
+
+        <h2 id="final-idea">Final Idea</h2>
+
+        <p style="text-align:justify">Ultimately, a breakthrough idea came from one of my best friends and colleagues, <a href="https://www.linkedin.com/in/xose-fern%C3%A1ndez-5a8064a3?originalSubdomain=es">Xose Fernánndez</a>, a fellow Cytotechnologist and Cytology Professor. He granted me access to an online microscope they use for teaching purposes, with a diverse range of scanned slides featuring different organs and diagnoses. After conducting research on their platform, I identified the organs and diagnoses that could provide the most data.</p>
+
+        <p style="text-align:justify">My initial data collection focused primarily on salivary gland samples, given the rich archive of slides and diagnostic diversity. Using my domain knowledge, I manually selected the fields of interest, employing the criteria used in Cytopathology for diagnosis. For instance, in the case of a Warthin Tumor, it was important to identify features such as a "dirty background" with lymphocytes and oncocytic cells.</p>
+
+        <p style="text-align:justify">Despite these efforts, the available data was still limited. To address this challenge, I used data augmentation techniques in Python to generate synthetic images, thereby augmenting the training and validation datasets.</p>
+
+        <p align="center">
+        <img src="https://imgtr.ee/images/2023/07/15/30eff73003bba45453f247e497ad7f50.png" width="36%">
+        <br>
+        <small><em>Synthetic Data of a Warthin Tumor, generated by Keras using ImageDataGenerator.</em></small>
+        </p>
+
+        <h2 id="final-notes">Final Notes</h2>
+
+        <p style="text-align:justify">Due to privacy concerns, I did not upload the final image folders to GitHub. Although ethical guidelines within academic cytology societies (of which I am a member) permit the use of individual images for research, education, and promoting cytology, strict protocols are followed in the medical community to ensure confidentiality and anonymity.</p>
+        </div>
+        ''', unsafe_allow_html=True)
+
+    elif page == 'Chapter III: Neural Networks':
+        st.markdown('''
+        <h1 id="about-neural-networks">About Neural Networks, Deep Learning and Image Multiclassification</h1>
+        <p align="center">
+          <img src="https://cdn.discordapp.com/attachments/1129416761922568212/1130370214043861022/isi.mube_an_open_spellbook_featuring_in_their_pages_neural_netw_397a026c-43bb-4da0-b8c1-ffe76f4b21eb.png" width="20%">
+        </p>
+        <p>To keep it nice, clean and easy to read, my code is structured in two major scripts which are designed to be reusable for different organ samples in an iterative process.</p>
+        ''', unsafe_allow_html=True)
+
+        st.header('Python Script I: Data Extracting, Cleaning and Pre-Processing')
+        st.markdown('''
+        The first script is responsible for data extraction, cleaning and preprocessing, which involves creating folder names with OS for different diagnoses per organ, training and validation subsets, copying images, and applying data augmentation techniques.
+        ''')
+        st.code('''
+        # Data Augmentation Parameters
+        datagen = ImageDataGenerator(
+            rescale=1.0/255,  # normalizes pixel values to [0, 1]
+            rotation_range=30, # cells can appear at any orientation
+            width_shift_range=.15, # cells can be located anywhere
+            height_shift_range=.15, # same as before, but vertically
+            horizontal_flip=True, # cell orientation doesn't matter
+            vertical_flip=True, # cell orientation doesn't matter
+            brightness_range=[0.5, 1.5],  # simulates variable lighting/staining
+            zoom_range=0.2  # simulates variable cell sizes/distances
+        )
+        ''', language='python')
+
+        st.markdown('I also saved 15% of the original data as unique copies for testing purposes.')
+
+        st.header('Python Script II: Deep Learning Modeling, Validation and Testing')
+        st.markdown('''
+        The second script is dedicated to building and validating the Deep Learning model, as well as testing its performance. I'll briefly explain the structure of the Convolutional Neural Network (CNN) used in this project:
+        ''')
+
+        st.code('''
+        x = layers.Conv2D(32, (3, 3), activation='relu')(inputs)
+        x = layers.MaxPooling2D((2, 2))(x)
+
+        x = layers.Conv2D(64, (3, 3), activation='relu')(x)
+        x = layers.MaxPooling2D((2, 2))(x)
+
+        x = layers.Conv2D(128, (1, 1), activation='relu')(x)
+        x = layers.MaxPooling2D((2, 2))(x)
+
+        x = layers.Conv2D(256, (2, 2), activation='relu')(x)
+        x = layers.MaxPooling2D((2, 2))(x)
+
+        x = layers.Flatten()(x)
+        x = layers.Dense(512, activation='relu')(x)
+        x = layers.Dense(256, activation='relu')(x)
+        x = layers.Dense(5, activation='softmax')(x)
+
+        return keras.Model(inputs, x)
+        ''', language='python')
+        st.subheader('Conv2 Layer')
+
+        st.markdown('''
+        Convolutional layer of the network, where the image pre-processing happens and the kernel filter the image on the whole image, detecting lines and edges. '32', '64', '128' refers to the number of filters used, starting from a small number. 'Relu' activation adds non-linearity to the model, to learn more complex patterns.
+        ''')
+
+        st.markdown('<p align="center"><img src="https://imgtr.ee/images/2023/07/16/2ec21fb98202f4f49aacede568160fef.webp" width="36%"></p>', unsafe_allow_html=True)
+
+        st.subheader('MaxPooling2D Layer')
+
+        st.markdown('''
+        It performs downsampling operations, and also spatial dimensions (width, height), reducing overfitting and the computational cost by decreasing the spatial dimensionality. This process goes along with the convolutional layer, with different kernel sizes, to learn more complex patterns from Data.
+        ''')
+
+        st.markdown('<p align="center"><img src="https://imgtr.ee/images/2023/07/16/2de962eb5ef3e02f776e63be21794789.png" width="36%"></p>', unsafe_allow_html=True)
+
+        st.subheader('Flatten Layer')
+
+        st.markdown('''
+        It prepares the multi-dimensional input for the last fully and dense layer, converting the 2D matrix to a 1D vector.
+        ''')
+
+        st.markdown('<p align="center"><img src="https://imgtr.ee/images/2023/07/16/09a1974ed212b1bdfaab538a6585d8ca.png" width="36%"></p>', unsafe_allow_html=True)
+
+        st.subheader('Dense Layer')
+
+        st.markdown('''
+        It takes the Flatten inputs through a series of neurons. First 512, then 256. It's where all previous neurons are fully connected, the 'softmax' activation it's used in multi-classification models, in this case, '5' corresponds to the number of target classes.
+        ''')
+
+        st.markdown('<p align="center"><img src="https://imgtr.ee/images/2023/07/16/1c1817cf8b06c0dbb8433e6d95357ad5.png" width="36%"></p>', unsafe_allow_html=True)
+
+        st.markdown('''
+        In short, this CNN model learns hierarchical representations of the images, from low-level structures (lines, edges, textures) in the initial layers to more complex high-level features such as patterns and objects.
+        ''')
